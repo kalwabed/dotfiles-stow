d40ecf23ba6e6fed6fadd037754220248385996c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-07-28 14:48:41 +0700

    wezterm font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,20 +1,28 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local opacity = 0.75
 
-config.window_background_opacity = 0.8
 config.color_scheme = "Gruvbox Dark (Gogh)"
+
 config.font = wezterm.font_with_fallback({
+	{ family = "Iosevka Term", stretch = "Expanded" },
+	"CaskaydiaCove Nerd Font Mono",
 	"Fira Code",
 	"JetBrains Mono",
 })
+config.font_size = 10
 
-config.font_size = 9
+-- Window
+config.window_background_opacity = opacity
+config.initial_rows = 45
+config.initial_cols = 180
 config.enable_scroll_bar = false
-config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
-config.initial_rows = 35
-config.initial_cols = 105
+-- Tabs
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
 -- config.window_padding = {
 -- 	left = 10,
